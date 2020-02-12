30899651e638dc0350e927d02b9288788f78c587
Ayodele Olufemi <tj5931qh@go.minnstate.edu>
2020-02-11 22:41:17 -0600

    added template

--- RetirementCalculator.py
+++ RetirementCalculator.py
@@ -5,3 +5,24 @@ Created on Tue Feb 11 19:43:13 2020
 @author: ayyor
 """
 
+import tkinter as tk
+
+HEIGHT = 1080
+WIDTH = 1920
+
+app = tk.Tk()
+canvas = tk.Canvas(app, height=HEIGHT, width=WIDTH, bg="#aecad0")
+canvas.pack()
+
+
+nav = tk.Frame(canvas, bg="#263D42")
+nav.place(relwidth=1, relheight=0.1)
+
+
+appName = tk.Label(nav, text="Retirement Calculator", font=("Helvetica 26 bold italic"), fg="white", bg="#263D42")  
+appName.pack(expand=True)
+
+
+
+
+app.mainloop()
